8f469ed5b128433e41d53c4e48b98053e4b1aa4b
andersonss <andersonfarin@gmail.com>
2013-03-09 12:00:23 -0300

    The Screen

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
-// Made for JamesM's tutorials 
+//           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
+
+#include "monitor.h"
 
 int main(struct multiboot *mboot_ptr)
 {
-    // All our initialisation calls will go in here.
-    return 0xDEADBABA;
+    // Initialise the screen (by clearing it)
+    monitor_clear();
+    // Write out a sample string
+    monitor_write("Hello, world!");
+    
+    return 0;
 }
