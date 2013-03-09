4187d9fd96538af55847f034f73b64ac8ba193d4
andersonss <andersonfarin@gmail.com>
2013-03-09 18:36:19 -0300

    Paging

---
+++ execute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./update_image.sh
+./run_bochs.sh
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
+#include "paging.h"
 
 int main(struct multiboot *mboot_ptr)
 {
@@ -12,13 +13,19 @@ int main(struct multiboot *mboot_ptr)
     // Initialise the screen (by clearing it)
     monitor_clear();
     // Write out a sample string
-    monitor_write("Hello, world!\n");
+    //monitor_write("Hello, world!\n");
 
-    asm volatile("int $0x3");
-    asm volatile("int $0x4");
+    //asm volatile("int $0x3");
+    //asm volatile("int $0x4");
 
-    asm volatile("sti");
-    init_timer(50);
+    //asm volatile("sti");
+    //init_timer(50);
+
+    initialise_paging();
+    monitor_write("Hello, paging world!\n");
+
+    u32int *ptr = (u32int*)0xA0000000;
+    u32int do_page_fault = *ptr;
 
     return 0;
 }
